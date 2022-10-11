9d9993360cefe4b53e2c8d20901fed722497e3e3
sourabhjo7 <sourabhjoshi9549408165@gmail.com>
2022-10-12 04:32:23 +0530

    pusher integrated and changing state simulteneously done

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,51 @@
+import React,{useState,useEffect} from 'react'
+import Pusher from 'pusher-js';
+import axios from "axios"
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
+  const [background,setBackground]= useState("#CDF6E5");
+  const [input,setInput]= useState("#CDF6E5");
+  useEffect(() => {
+    const pusher = new Pusher(process.env.REACT_APP_KEY
+      ,{
+      cluster: process.env.REACT_APP_CLUSTER
+     });
+    const channel = pusher.subscribe("client");
+    channel.bind("suno-client", function(data) {
+      setBackground(data.color)
+      console.log("data==>",data);
+     });
+      }, [])
+  
+  const handleClick=async(e)=>{  
+      // requesting server to do realtime trigger
+      setBackground(input);
+      await axios.post('http://localhost:5000/update',{color:input});
+     
+  }
+    
+    return (
+      <div style={ {backgroundColor:background} } className="App">
       <div className="midContainer">
       <h2 id="inlabel">Choose Your Color</h2>
       <div className="inputCon">
      
-      <input type="color"  
-            value="#f6b73c"/>
+      <input type="color" 
+      onChange={(e)=>{
+              setInput(e.target.value);
+            }}
+             value={input}/>
       </div>
-     
-      <button className="btn" type="button"> Change</button>
+      <button onClick={ handleClick} className="btn" type="button"> Change</button>
      
       </div>
     </div>
-  );
+    )
+
 }
 
 export default App;
+
+
+
--- server/app.js
+++ server/app.js
@@ -1,7 +1,55 @@
 const  express = require('express')
-const app = express()
+const app = express();
+const cors=require("cors");
 const port = 5000;
+const Pusher=require("pusher")
+require("dotenv").config();
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use(cors({
+    origin:["http://localhost:3000"],
+    optionsSuccessStatus: 200,
+    credentials: true
+}));
+app.use(express.json());
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+const pusher = new Pusher({
+    appId: process.env.APPID,
+    key: process.env.KEY,
+    secret: process.env.SECRET,
+    cluster: process.env.CLUSTER,
+    useTLS: true
+  });
+  app.use(function(req, res, next) {
+    res.header('Content-Type', 'application/json;charset=UTF-8')
+    res.header('Access-Control-Allow-Credentials', true)
+    res.header(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept'
+    )
+    next()
+  })
+
+  // routes
+app.get('/', (req, res) =>{
+    res.json({
+        success:true
+      });
+} );
+app.post("/update",async(req,res)=>{
+    try{
+        await pusher.trigger("client", "suno-client", {
+            color: req.body.color
+          });
+          return res.json({
+            success:true
+          });
+    }
+    catch(e){
+        return res.json({
+            success:false
+          });
+    }
+  
+})
+
+app.listen( port, () => console.log(`Example app listening on port ${port}!`))
